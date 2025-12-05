d7da800c0a1b3f4940b925ea0ce721e21bd17aa2
Omar Saber Talaat Abdelrahman <omar.abdelrahman.de@gmail.com>
2025-12-05 17:46:56 +0100

    feat: day-two part-one

---
+++ 2025/day-two/src/main.rs
@@ -0,0 +1,66 @@
+use std::{ops::RangeInclusive, panic};
+
+fn is_even_size(n: &u64) -> bool {
+    !n.ilog10().is_multiple_of(2)
+}
+
+fn is_invalid(n: &u64) -> bool {
+    let size = n.ilog10() + 1;
+    let d = 10_u64.pow(size / 2);
+    (n / d) == (n % d)
+}
+
+struct IdRange {
+    range: RangeInclusive<u64>,
+}
+
+impl IdRange {
+    fn new(b: &u64, e: &u64) -> Self {
+        IdRange {
+            range: RangeInclusive::new(*b, *e),
+        }
+    }
+
+    fn empty() -> Self {
+        IdRange {
+            range: RangeInclusive::new(1, 0),
+        }
+    }
+
+    fn invalids_count(self) -> u64 {
+        self.range.filter(is_even_size).filter(is_invalid).sum()
+    }
+}
+
+impl From<(&str, &str)> for IdRange {
+    fn from(value: (&str, &str)) -> Self {
+        let b = value
+            .0
+            .trim()
+            .parse::<u64>()
+            .unwrap_or_else(|_| panic!("Failed to parse {}", value.0));
+        let e = value
+            .1
+            .trim()
+            .parse::<u64>()
+            .unwrap_or_else(|_| panic!("Failed to parse {}", value.1));
+        IdRange::new(&b, &e)
+    }
+}
+
+fn main() -> std::io::Result<()> {
+    let invalids = std::fs::read_to_string("src/in.txt").map(|ranges| {
+        ranges
+            .split(',')
+            .map(|range| {
+                range
+                    .split_once('-')
+                    .map(IdRange::from)
+                    .unwrap_or_else(IdRange::empty)
+                    .invalids_count()
+            })
+            .sum::<u64>()
+    })?;
+    println!("{}", invalids);
+    Ok(())
+}
