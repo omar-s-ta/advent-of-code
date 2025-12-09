dfb5598c0f3496cce7af67872a0cbdc25fd71627
Omar Saber Talaat Abdelrahman <omar.abdelrahman.de@gmail.com>
2025-12-09 23:13:02 +0100

    refactor: use more fp style rust

--- 2025/day-one/src/main.rs
+++ 2025/day-one/src/main.rs
@@ -2,45 +2,61 @@ use std::io::BufRead;
 
 const MOD: u16 = 100;
 
+#[derive(Debug, Clone, Copy)]
+enum Direction {
+    Left,
+    Right,
+}
+
+impl Direction {
+    fn from_str(s: &str) -> Option<Self> {
+        match s {
+            "L" => Some(Direction::Left),
+            "R" => Some(Direction::Right),
+            _ => None,
+        }
+    }
+}
+
+#[derive(Default)]
 struct Tracker {
     value: u16,
     result: u16,
 }
 
 impl Tracker {
-    pub fn new() -> Self {
-        Tracker {
+    fn new() -> Self {
+        Self {
             value: 50,
-            result: 0,
+            ..Default::default()
         }
     }
 
     #[allow(dead_code)]
-    pub fn advance_pt_one(&mut self, dir: &str, offset: &u16) {
+    fn advance_pt_one(&mut self, dir: Direction, offset: u16) {
+        let offset = offset.rem_euclid(MOD);
         match dir {
-            "L" => self.sub(&offset.rem_euclid(MOD)),
-            "R" => self.add(&offset.rem_euclid(MOD)),
-            _ => panic!("no good"),
+            Direction::Left => self.sub(offset),
+            Direction::Right => self.add(offset),
         }
     }
 
-    pub fn advance_pt_two(&mut self, dir: &str, offset: &u16) {
+    fn advance_pt_two(&mut self, dir: Direction, offset: u16) {
         let (rem, cycles) = (offset % MOD, offset / MOD);
         match dir {
-            "L" => self.sub_pt_two(&rem, &cycles),
-            "R" => self.add_pt_two(&rem, &cycles),
-            _ => panic!("no good"),
+            Direction::Left => self.sub_pt_two(rem, cycles),
+            Direction::Right => self.add_pt_two(rem, cycles),
         }
     }
 
     #[inline]
-    fn add(&mut self, offset: &u16) {
+    fn add(&mut self, offset: u16) {
         self.value = (self.value + offset).rem_euclid(MOD);
         self.tick_on_zero();
     }
 
     #[inline]
-    fn sub(&mut self, offset: &u16) {
+    fn sub(&mut self, offset: u16) {
         self.value = (self.value + MOD - offset).rem_euclid(MOD);
         self.tick_on_zero();
     }
@@ -53,7 +69,7 @@ impl Tracker {
     }
 
     #[inline]
-    fn add_pt_two(&mut self, rem: &u16, cycles: &u16) {
+    fn add_pt_two(&mut self, rem: u16, cycles: u16) {
         self.value += rem;
         if self.value >= MOD {
             self.value -= MOD;
@@ -63,8 +79,8 @@ impl Tracker {
     }
 
     #[inline]
-    fn sub_pt_two(&mut self, rem: &u16, cycles: &u16) {
-        if self.value > 0 && self.value <= *rem {
+    fn sub_pt_two(&mut self, rem: u16, cycles: u16) {
+        if self.value > 0 && self.value <= rem {
             self.result += 1;
         }
         self.value = (self.value + MOD - rem).rem_euclid(MOD);
@@ -73,20 +89,18 @@ impl Tracker {
 }
 
 fn main() -> std::io::Result<()> {
-    let file = std::fs::File::open("src/in.txt")?;
-    let reader = std::io::BufReader::new(file);
-
-    let mut tracker = Tracker::new();
-    reader.lines().for_each(|line| {
-        line.iter().for_each(|cmd| {
-            let (dir, offset) = cmd.split_at(1);
-            offset
-                .parse::<u16>()
+    let tracker = std::io::BufReader::new(std::fs::File::open("src/in.txt")?)
+        .lines()
+        .map_while(Result::ok)
+        .fold(Tracker::new(), |mut tracker, cmd| {
+            let (dir_str, offset_str) = cmd.split_at(1);
+            Direction::from_str(dir_str)
+                .and_then(|dir| offset_str.parse::<u16>().ok().map(|offset| (dir, offset)))
                 .iter()
-                .for_each(|u| tracker.advance_pt_two(dir, u));
+                .for_each(|(dir, offset)| tracker.advance_pt_two(*dir, *offset));
+            tracker
         });
-    });
-    println!("{}", tracker.result);
 
+    println!("{}", tracker.result);
     Ok(())
 }
